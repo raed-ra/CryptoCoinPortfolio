f4d71ece0146607f58c181430cad748cf96c17db
Raed <raed_ra@yahoo.com>
2020-10-19 20:29:15 +0800

    53

--- server.js
+++ server.js
@@ -1,69 +1,3 @@
-// const express = require("express");
-// const session = require("express-session");
-// const bodyParser = require("body-parser"); // form data
-// const cookieParser = require("cookie-parser");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-// //const corsConfig = require('./config/cors');
-// const passport = require("passport");
-// //const connectDb = require("./config/database");
-
-// const MongoStore = require("connect-mongo")(session);
-// //const routes = require("./routes");
-// dotenv.config({ path: ".env" });
-
-
-// const app = express();
-
-// connectDb();
-
-// // Configure body parsing for AJAX requests
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cookieParser(process.env.SESSION_SECRET));
-
-// app.use(cors(corsConfig));
-
-// const PORT = process.env.PORT || 3001;
-
-// // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-
-
-// app.use(
-//   session({
-//       resave: true,
-//       saveUninitialized: true,
-//       secret: process.env.SESSION_SECRET,
-//       cookie: {
-//           secure: false, // not using https
-//           maxAge: 1209600000,
-//       }, // two weeks in milliseconds
-//       store: new MongoStore({
-//           url: process.env.MONGODB_URI,
-//           autoReconnect: true,
-//       }),
-//   })
-// );
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Define API routes here
-// //app.use('/api',  routes);
-
-// // Send every other request to the React app
-// // Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`🌎 ==> API server now on port ${PORT}!`);
-// });
-
 const express = require("express");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
@@ -86,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.SESSION_SECRET));
 
-app.use(cors(corsConfig));
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -114,11 +48,13 @@ app.use(passport.session());
 // Define API routes here
 app.use('/api',  routes);
 
+//app.use(cors(corsConfig));
+
 // Send every other request to the React app
 // Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
