5739d9d2cc3b3c89dd728b48b0bc309e3d37b7a7
Raed <raed_ra@yahoo.com>
2020-10-19 19:39:36 +0800

    52

--- server.js
+++ server.js
@@ -83,10 +83,10 @@ connectDb();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json(corsConfig));
+app.use(express.json());
 app.use(cookieParser(process.env.SESSION_SECRET));
 
-app.use(cors());
+app.use(cors(corsConfig));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -116,9 +116,9 @@ app.use('/api',  routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
