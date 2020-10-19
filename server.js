cc2d2ffe339b37874e6d0c5b2438ff3b7127b6ef
Raed <raed_ra@yahoo.com>
2020-10-19 17:30:06 +0800

    36

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Define API routes here
-// app.use('/api',  routes);
+app.use('/api',  routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
